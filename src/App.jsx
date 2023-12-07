9c7c13efeef575a26fa885751c73b6011a3615c2
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-12-07 22:03:22 +0500

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-       
-      </div>
-      <h1>Translater</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+     
+      
     </>
   )
 }
